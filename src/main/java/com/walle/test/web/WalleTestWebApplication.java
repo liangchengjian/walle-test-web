864afdae3de952ce9f72e78f3436820994976c2e
liangchengjian <liangchengjian@carelink.com>
2019-07-02 15:10:08 +0800

    初始化

---
+++ src/main/java/com/walle/test/web/WalleTestWebApplication.java
@@ -0,0 +1,13 @@
+package com.walle.test.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalleTestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalleTestWebApplication.class, args);
+	}
+
+}
